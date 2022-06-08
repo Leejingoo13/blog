c5de585fd620e6244bc9146ad751442c3fe7ac7f
Leejingoo13 <dlwlsrn14@gmail.com>
2022-06-07 17:19:00 -0700

    테스트 커밋

---
+++ src/test/java/com/hi/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hi.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
